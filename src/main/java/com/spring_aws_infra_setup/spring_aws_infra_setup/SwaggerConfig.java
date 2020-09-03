9df6655a86f0160866442bfad8d0762166bb0ddf
prabhat1432 <prabhat.malhotra@thoughtworks.com>
2020-09-03 23:05:52 +0530

    [Prabhat] adds swagger UI for apis

---
+++ src/main/java/com/spring_aws_infra_setup/spring_aws_infra_setup/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.spring_aws_infra_setup.spring_aws_infra_setup;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.function.Predicate;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(Predicate.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder().title("Spring boot with swagger")
+                .description("This is just an implementation of swagger with spring boot")
+                .version("V1.0")
+                .build();
+    }
+}
+
